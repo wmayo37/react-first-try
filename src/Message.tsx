31d3255b36b2e9fe1993dd590466275e71cc7c53
Will Mayo <wm128@duke.edu>
2025-04-14 20:55:04 -0400

    first commit

---
+++ src/Message.tsx
@@ -0,0 +1,10 @@
+function Message() {
+    //JSX = Javascript XML
+    const name = 'Will';
+    if(name)
+        return <h1>Hello {name}</h1>;
+    return <h1>Hello World</h1>;
+
+}
+
+export default Message;
