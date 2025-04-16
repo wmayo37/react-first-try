b4e14f265483e2dcd5ae08351db209dc12afb500
Will Mayo <wm128@duke.edu>
2025-04-16 05:12:54 -0400

    added close button and popup to alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
   return (
     <div>
-      <Button onClick={() => console.log("Clicked")}>My Button</Button>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>
+          Hey wait a second...
+        </Alert>
+      )}
+      <Button onClick={() => setAlertVisibility(true)}>My Button</Button>
     </div>
   );
 }
