a29f767e1fcc21673a1a87d865ea001db697815d
Will Mayo <wm128@duke.edu>
2025-04-16 03:30:04 -0400

    passing children

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,3 +1,11 @@
-function Alert() {}
+import React, { ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+}
+
+const Alert = ({ children }: Props) => {
+  return <div className="alert alert-primary">{children}</div>;
+};
 
 export default Alert;
